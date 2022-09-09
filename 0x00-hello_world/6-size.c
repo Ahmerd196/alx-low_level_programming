41c1f1dee5b65d5b15014ca9077e8aa72ec339b3
Ahmerd196 <Ahmerd196@gmail.com>
2022-09-09 08:39:37 -0700

    C-types printf-commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,14 +8,10 @@
  */
 int main(void)
 {
-	printf("size of type int: %ld bytes\n", sizeof(int));
-	printf("size of type int: %ld bytes\n", sizeof(char));
-	printf("size of type int: %ld bytes\n", sizeof(float));
-	printf("size of type int: %ld bytes\n", sizeof(long int));
-	printf("size of type int: %ld bytes\n", sizeof(double));
-	printf("size of type int: %ld bytes\n", sizeof(short int));
-	printf("size of type int: %ld bytes\n", sizeof(signed char));
-	printf("size of type int: %ld bytes\n", sizeof(long unsigned int));
-
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
 	return (0);
 }
