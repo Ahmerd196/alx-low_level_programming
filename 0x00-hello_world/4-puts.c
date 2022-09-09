32b620cb44078bbdcd71ba70f6384109fa48242c
Ahmerd196 <Ahmerd196@gmail.com>
2022-09-09 06:11:13 -0700

    C-puts-commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include<stdio.h>
 /**
- *main-entry point
+ *main - Entry point
  *
  *
- * return 0 always success
+ * Return: Always 0 (Success)
  */
-int main(void){
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
