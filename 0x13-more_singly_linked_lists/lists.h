8e6f5d7cec521be507dac0ab8a7005c95fba6cad
Ahmerd196 <Ahmerd196@gmail.com>
2022-10-24 20:43:25 -0700

    C-prog more on single link

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,5 +1,4 @@
 #ifndef _LISTS_
-
 #define _LISTS_
 
 
@@ -45,6 +44,7 @@ typedef struct listp_s
 	void *p;
 	struct listp_s *next;
 } listp_t;
+
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
