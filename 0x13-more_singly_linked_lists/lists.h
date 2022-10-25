603b358caa13e8779d2c50afecf0803383dff3ac
Ahmerd196 <Ahmerd196@gmail.com>
2022-10-24 20:55:48 -0700

    C-prog more on single link

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,14 +1,7 @@
 #ifndef _LISTS_
 #define _LISTS_
-
-
-
 #include <stdio.h>
-
 #include <stdlib.h>
-
-
-
 /**
  * struct listint_s - singly linked list
  *
@@ -16,12 +9,9 @@
  *
  * @next: points to the next node
  *
- *
- *
  * Description: singly linked list node structure
  *
  * for Holberton project
- *
  */
 typedef struct listint_s
 {
@@ -35,7 +25,7 @@ typedef struct listint_s
  *
  * @next: points to the next node
  *
- * Description: singly linked list of pointers
+ * Description: singly linked list node pointers
  *
  */
 
