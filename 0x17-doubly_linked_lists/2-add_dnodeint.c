daf8e2de03cd6cecb1b5d0087372536cac262e32
Ahmerd196 <Ahmerd196@gmail.com>
2022-12-08 13:38:50 -0800

    continuation of C programming

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,4 +1,4 @@
-nclude "lists.h"
+#include "lists.h"
 /**
  * add_dnodeint - adds a new node at the beginning
  *
@@ -9,6 +9,7 @@ nclude "lists.h"
  * @n: value of the element
  *
  * Return: the address of the new element
+ *
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
