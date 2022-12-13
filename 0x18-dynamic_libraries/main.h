0c52fb0b8ec3457f81d045594566cfe172120d75
Ahmerd196 <Ahmerd196@gmail.com>
2022-12-13 15:48:53 -0800

    C programming continuation

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,10 +1,11 @@
 #ifndef _MAIN_H
 #define _MAIN_H
+
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
 int _abs(int n);
-int _isupper(int c);
+int _isupper(intc);
 int _isdigit(int c);
 int _strlen(char *s);
 void _puts(char *s);
